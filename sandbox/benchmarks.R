a96e95e2bff1af19cca2847e8d3395ef2579a85c
antfioravanti <antoniofioravanti4@gmail.com>
2025-02-03 15:22:03 +0100

    New Branch with C++ functions

---
+++ sandbox/benchmarks.R
@@ -0,0 +1,77 @@
+#-------------------------------------------------------------------------------
+### SPATIAL PROCESS SIMULATION ###
+#-------------------------------------------------------------------------------
+options(scipen = 5) # some decimals, set to 999 for max decimals
+#options(scipen = 0) # scientific display
+if (!require(sp)) install.packages("sp"); library(sp)
+if (!require(tidyr)) install.packages("tidyr"); library(tidyr)
+if (!require(dplyr)) install.packages("dplyr"); library(dplyr)
+if (!require(gstat)) install.packages("gstat"); library(gstat)
+if (!require(MASS)) install.packages("MASS"); library(MASS)
+if (!require(fields)) install.packages("fields"); library(fields)
+if (!require(rstudioapi)) install.packages("rstudioapi"); library(rstudioapi)
+if (!require(pracma)) install.packages("pracma"); library(pracma)
+if (!require(parallel)) install.packages("parallel"); library(parallel)
+if (!require(reshape2)) install.packages("reshape2"); library(reshape2)
+if (!require(ncf)) install.packages("ncf"); library(ncf)
+if (!require(openxlsx)) install.packages("openxlsx"); library(openxlsx)
+if (!require(Rcpp)) install.packages("Rcpp"); library(Rcpp)
+if (!require(microbenchmark)) install.packages("microbenchmark"); library(microbenchmark)
+#if (!require(here)) install.packages("here"); library(here)
+# Set the working directory to where the current file is saved
+setwd(file.path(dirname(rstudioapi::getActiveDocumentContext()$path),".."))
+wd = file.path(dirname(rstudioapi::getActiveDocumentContext()$path))
+source("R/utils.R") # General functions
+source("R/covariance_funs.R") # Analytical covariances
+source("R/estimators.R") # Estimators
+source("R/plotting.R")  # Plotting functions
+
+sourceCpp("src/estimators.cpp")
+set.seed(42)
+#-------------------------------------------------------------------------------
+
+nvec <- c(30,30)
+
+
+# Benchmark both versions
+benchmark_results <- microbenchmark(
+  R_version = compute_m_values(5, 3, nvec, d = 1, flipsign = TRUE, flipposition = FALSE),
+  Cpp_version = compute_m_values_cpp(5, 3, nvec, d = 1, flipsign = TRUE, flipposition = FALSE),
+  times = 10000
+)
+
+print(benchmark_results)
+
+# Test input
+i <- 5
+j <- 3
+nvec <- c(20,20)
+d <- 1
+flipsign <- TRUE
+flipposition <- FALSE
+
+# Run the R function
+result_r <- compute_m_values(i, j, nvec, d, flipsign, flipposition)
+
+# Run the C++ function
+result_cpp <- compute_m_values_cpp(i, j, nvec, d, flipsign, flipposition)
+
+
+i <- 5
+j <- 3
+
+d <- 1
+flipsign <- TRUE
+flipposition <- FALSE
+
+
+k = -20
+s = 2
+d = 1
+m_fun(k = k, s= s,d=1, nvec=nvec)
+m_fun_cpp(k = k, s=s, d=1, nvec=nvec)
+
+
+
+
+
---
+++ src/estimators.cpp
@@ -0,0 +1,93 @@
+// ----------------------------------------------------------------------------
+// Spatial Covariance Estiamtors
+// Author: Antonio Fioravanti
+// Date: 2025-01-31
+// ----------------------------------------------------------------------------
+
+// Header for integration between R and C++
+#include <Rcpp.h>
+#include <algorithm> // for std::reverse
+#include <cmath>      // for std::ceil
+
+// Use the Rcpp namespace to avoid prefixing Rcpp classes
+using namespace Rcpp;
+
+//-----------------------------------------------------------------------------
+// M FUNCTIONS
+//-----------------------------------------------------------------------------
+
+// [[Rcpp::export]]
+int m0_fun_cpp(int k, int d = 1){
+    // M0 function
+    return ((k -1) % d) +1;
+}
+
+// [[Rcpp::export]]
+int m_fun_cpp(int k, int s, int d, NumericVector nvec){
+    // Parameters:
+    // k - value
+    // s - spatial dimension counter (1,...,g)
+    // d - dimension of a potentially multivariate process (default should be 1)
+    // nvec - vector containing the sizes of the spatial grid
+
+    if (s == 1){
+        // Calculate the ceiling of k/d and apply the modulus operation
+        int result = ((int)std::ceil((double)k/d) - 1) % (d * (int)nvec[0])+ 1;
+        return result;
+    } else {
+        // Calculate the product of the previous dimensions nvec[1:(s-1)]
+        double prod_previous_n = 1.0;
+        for (int idx = 0; idx < (s-1); ++idx) {
+            prod_previous_n *= nvec[idx];
+        }
+        // Ceiling and modulus operations for the case s > 1
+        int result = ((int)std::ceil((double)k / (d * prod_previous_n)) -1) % 
+                     (d * (int)(prod_previous_n * nvec[s - 1])) + 1;
+        return result;
+    }
+}
+
+// [[Rcpp::export]]
+NumericVector compute_m0_values_cpp(int i, int j, int d = 1){
+    // Call m0_fun_cpp to compute m0 values for i and j
+    int m0_i = m0_fun_cpp(i,d);
+    int m0_j = m0_fun_cpp(j, d);
+
+    // Create NumericVector to store the results
+    NumericVector m0_ij = NumericVector::create(m0_i, m0_j);
+
+    // Return the vector
+    return m0_ij;
+}
+
+// [[Rcpp::export]]
+NumericVector compute_m_values_cpp(int i, int j, NumericVector nvec, 
+                                   int d =1,
+                                   bool flipsign = true, 
+                                   bool flipposition = false){
+    
+    // Determine the number of spatial dimension
+    int g = nvec.size();
+
+    // Initialize out vector
+    NumericVector m_ij(g);
+
+    // Compute the differences using m_fun_cpp
+    for (int s = 1; s <= g; s++){
+        m_ij[s - 1] = (i - 1) % (d * static_cast<int>(nvec[s - 1]))+ 1 -
+                      ((j - 1) % (d * static_cast<int>(nvec[s - 1])) + 1);
+    }
+    // If flipsign is true, multiply each element by -1
+    if (flipsign){
+        for (int idx = 0; idx < g; ++idx){
+            m_ij[idx] = -m_ij[idx];
+        }
+    }
+
+    // If flip position is true, reverse the vector
+    if (flipposition){
+        std::reverse(m_ij.begin(), m_ij.end());
+    }
+
+    return m_ij;
+}
